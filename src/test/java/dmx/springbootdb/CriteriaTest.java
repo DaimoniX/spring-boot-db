e3fecd72d176f4b4490b772dac5d1cc100d343cc
Daimonix <dimaprotsap@gmail.com>
2024-02-13 18:29:12 +0200

    Fixed criteria tests

--- src/test/java/dmx/springbootdb/CriteriaTest.java
+++ src/test/java/dmx/springbootdb/CriteriaTest.java
@@ -38,6 +38,10 @@ public class CriteriaTest {
         save(new TestEntity("T3", 3));
         save(new TestEntity("T4", 4));
         save(new TestEntity("T5", 5));
+    }
+
+    @BeforeEach
+    public void cleanCriteria() {
         criteriaBuilder = entityManager.getCriteriaBuilder();
         entityQuery = criteriaBuilder.createQuery(TestEntity.class);
     }
@@ -54,7 +58,7 @@ public class CriteriaTest {
     public void criteriaTest() {
         final var selectAllQuery = entityQuery.select(entityQuery.from(TestEntity.class));
         final var result = entityManager.createQuery(selectAllQuery).getResultList();
-        assertThat(result).asList().hasSize(5);
+        assertThat(result).asList().hasSize(6);
     }
 
     @Test
@@ -67,16 +71,12 @@ public class CriteriaTest {
 
     @Test
     public void criteriaTestSort() {
-        final var em = entityManagerFactory.createEntityManager();
-        final var cb = em.getCriteriaBuilder();
-        final var eq = cb.createQuery(TestEntity.class);
+        final var root = entityQuery.from(TestEntity.class);
+        final var order = criteriaBuilder.desc(root.get("id"));
+        final var selectSortQuery = entityQuery.orderBy(order);
+        final var result = entityManager.createQuery(selectSortQuery).getResultList();
 
-        final var root = eq.from(TestEntity.class);
-        final var order = cb.desc(root.get("id"));
-        final var selectSortQuery = eq.orderBy(order);
-        final var result = em.createQuery(selectSortQuery).getResultList();
-
-        assertThat(result).asList().extracting("id").containsExactly(5, 4, 3, 2, 1);
+        assertThat(result).asList().extracting("id").containsExactly(6, 5, 4, 3, 2, 1);
     }
 
     // gt = greater than
