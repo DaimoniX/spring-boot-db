9e16dc81f23eebb7baa8a654169968a081ead42d
Daimonix <dimaprotsap@gmail.com>
2024-03-08 17:16:15 +0200

    Practice 6

--- src/main/java/dmx/springbootdb/dao/Product.java
+++ src/main/java/dmx/springbootdb/dao/Product.java
@@ -6,8 +6,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Table(name = "product")
 @Entity
+@Table(name = "product", indexes = {
+        @Index(name = "name_index", columnList = "name"),
+        @Index(name = "complex_index", columnList = "name, val"),
+        @Index(name = "unique_name_index", columnList = "name", unique = true)
+})
 @NoArgsConstructor
 public class Product {
     @Id
@@ -16,6 +20,7 @@ public class Product {
     @NotBlank
     private String name;
     private int val;
+    private double weight;
 
     public Product(String name, int val) {
         this.name = name;
